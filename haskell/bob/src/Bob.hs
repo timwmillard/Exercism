2907d9647f1045bdc177e7ea84bcb5d52c405fc4
Tim Millard <tim@rapidpaylegal.com>
2023-08-07 23:05:52 +1000

    Solved Haskell Bob

--- haskell/bob/src/Bob.hs
+++ haskell/bob/src/Bob.hs
@@ -1,4 +1,23 @@
 module Bob (responseFor) where
 
+import Data.Char (isSpace)
+
+trim :: String -> String
+trim = f . f
+   where f = reverse . dropWhile isSpace
+
 responseFor :: String -> String
-responseFor xs = error "You need to implement this function."
+responseFor ask
+  | xs == "" = "Fine. Be that way!"
+  | isQuestion && isYelling = "Calm down, I know what I'm doing!"
+  | isQuestion = "Sure."
+  | isYelling = "Whoa, chill out!"
+  | otherwise = "Whatever."
+  where
+    xs = trim ask
+    isQuestion = last xs == '?'
+    isYelling = 
+        all (`notElem` ['a'..'z']) xs
+        && 
+        any (`elem` ['A'..'Z']) xs 
+
