5e5db5e3e2196ed0a537d9f64b8a30c52a1702df
Tim Millard <tim@rapidpaylegal.com>
2023-08-07 10:56:00 +1000

    Solved Haskell Space Age

--- haskell/space-age/src/SpaceAge.hs
+++ haskell/space-age/src/SpaceAge.hs
@@ -10,4 +10,14 @@ data Planet = Mercury
             | Neptune
 
 ageOn :: Planet -> Float -> Float
-ageOn planet seconds = error "You need to implement this function."
+ageOn planet seconds =
+    case planet of
+        Mercury -> age / 0.2408467
+        Venus -> age /  0.61519726
+        Earth -> age / 1.0
+        Mars -> age / 1.8808158
+        Jupiter -> age / 11.862615
+        Saturn -> age / 29.447498
+        Uranus -> age / 84.016846
+        Neptune -> age / 164.79132
+    where age = seconds / 365.25 / 24 / 60 / 60
