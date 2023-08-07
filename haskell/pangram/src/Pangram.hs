cba08ec39bf2a935aeede62e8b214c9d710659e8
Tim Millard <tim@rapidpaylegal.com>
2023-08-07 14:06:59 +1000

    Solve Haskell Pangram

--- haskell/pangram/src/Pangram.hs
+++ haskell/pangram/src/Pangram.hs
@@ -1,4 +1,13 @@
 module Pangram (isPangram) where
 
-isPangram :: String -> Bool
-isPangram text = error "You need to implement this function."
+import Data.Char 
+
+-- isPangram :: String -> Bool
+-- isPangram text = map toLower text == ""
+
+-- map (\x -> elem x (lowerPangram "The Quick brown fox jumps over the Lazy Dog")) allLetters
+isPangram :: String -> Bool 
+isPangram text = 
+     foldr (&&) True $ map (\x -> elem x (map toLower text)) allLetters
+
+allLetters = ['a'..'z']
