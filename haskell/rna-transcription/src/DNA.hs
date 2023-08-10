964e189dfb03e6b9c2bde2e2a7706e7f2f1c526b
Tim Millard <tim@rapidpaylegal.com>
2023-08-10 23:05:45 +1000

    wip Haskell RNA Transcription

--- haskell/rna-transcription/src/DNA.hs
+++ haskell/rna-transcription/src/DNA.hs
@@ -1,4 +1,24 @@
 module DNA (toRNA) where
 
+
 toRNA :: String -> Either Char String
-toRNA xs = error "You need to implement this function."
+toRNA "" = Right ""
+toRNA (x : xs) = Right (r : toRNA xs)
+  where
+    r = case x of
+      'G' -> 'C'
+      'C' -> 'G'
+      'T' -> 'A'
+      'A' -> 'U'
+      _ -> 'X'
+
+-- toRNA2 :: String -> String
+-- toRNA2 "" = ""
+-- toRNA2 (x : xs) = r : toRNA2 xs
+--   where
+--     r = case x of
+--       'G' -> 'C'
+--       'C' -> 'G'
+--       'T' -> 'A'
+--       'A' -> 'U'
+--       _ -> 'X'
