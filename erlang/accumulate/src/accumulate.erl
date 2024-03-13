0a0c338fe50c857b1a0ada6fa55728a873850dfb
Tim Millard <timwmillard@gmail.com>
2024-03-13 22:24:54 +1100

    Erlang accumulate alternative version
    Uses list comprehension

--- erlang/accumulate/src/accumulate.erl
+++ erlang/accumulate/src/accumulate.erl
@@ -1,15 +1,22 @@
 -module(accumulate).
--export([accumulate/2]).
+-export([accumulate/2, accumulate2/2]).
 
 %%
 %% given a fun and a list, apply fun to each list item replacing list item with fun's return value.
 %%
+%% Solution uses list comprehension
 -spec accumulate(fun((A) -> B), list(A)) -> list(B).
 accumulate(Fn, List) -> 
-    accumulate(Fn, List, []).
+    [Fn(X) || X <- List].
 
-accumulate(_Fn, [], AccList) -> AccList;
-accumulate(Fn, [Head|Rest], AccList) -> 
+
+% use recuration to accumulate the list
+-spec accumulate2(fun((A) -> B), list(A)) -> list(B).
+accumulate2(Fn, List) -> 
+    accumulate2(Fn, List, []).
+
+accumulate2(_Fn, [], AccList) -> AccList;
+accumulate2(Fn, [Head|Rest], AccList) -> 
     X = Fn(Head),
-    accumulate(Fn, Rest, lists:append(AccList, [X])).
+    accumulate2(Fn, Rest, lists:append(AccList, [X])).
 
