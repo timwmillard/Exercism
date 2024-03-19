82dc5588cd8704fce76a6d326f6920041cf0c2a5
Tim Millard <timwmillard@gmail.com>
2024-03-20 00:26:36 +1100

    Solved erlang collatz-conjecture

--- erlang/collatz-conjecture/src/collatz_conjecture.erl
+++ erlang/collatz-conjecture/src/collatz_conjecture.erl
@@ -3,11 +3,11 @@
 -export([steps/1]).
 
 
-steps(N) -> steps(N, 0).
+steps(N) when N > 0 -> steps(N, 0).
 
 steps(1, Steps) -> Steps;
 steps(N, Steps) when N rem 2 == 0 ->
-    steps(N/2, Steps+1);
+    steps(N div 2, Steps+1);
 steps(N, Steps) ->
     steps(N*3 + 1, Steps+1).
 
