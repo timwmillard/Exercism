bfec59a0060915d73ac3136a8fd3445700ed82cf
Tim Millard <timwmillard@gmail.com>
2024-03-23 14:04:43 +1100

    Improved version, C armstrong number

--- c/armstrong-numbers/armstrong_numbers.c
+++ c/armstrong-numbers/armstrong_numbers.c
@@ -1,6 +1,4 @@
-#include <stdio.h>
 #include <math.h>
-#include <stdlib.h>
 #include <stdbool.h>
 
 #include "armstrong_numbers.h"
@@ -8,25 +6,14 @@
 
 bool is_armstrong_number(int candidate)
 {
-    if (candidate < 0 ) {
-        return false;
-    }
-    if (candidate == 0 ) {
-        return true;
-    }
-
     int num_digits = (int)log10(candidate) + 1;
 
-    char *digits = malloc(num_digits + 1);
-    snprintf(digits, num_digits+1, "%d", candidate);
-
     int sum = 0;
-    for (int i=0; i<num_digits; i++) {
-        int digit = digits[i] - '0';
+    int rem = candidate;
+    while (rem > 0) {
+        int digit = rem % 10;
         sum += pow(digit, num_digits);
+        rem /= 10;
     }
-
-    free(digits);
-
     return sum == candidate;
 }
