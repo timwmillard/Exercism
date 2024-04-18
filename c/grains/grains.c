699f8a2d133c43dca358ecbd757c528b4f84a89f
Tim Millard <timwmillard@gmail.com>
2024-04-18 23:05:09 +1000

    C grains update version 2

--- c/grains/grains.c
+++ c/grains/grains.c
@@ -1,22 +1,13 @@
-#include <math.h>
-
 #include "grains.h"
-
-const int table[64] = {
-    1,
-};
 
 uint64_t square(uint8_t index)
 {
     if (index == 0 || index > 64) return 0;
-    return pow(2, --index);
+    return (uint64_t)1<<(--index);
 }
 
 uint64_t total(void)
 {
-    int sum = 0;
-    for (int i=1; i < 64; i++)
-        sum += square(i);
-    return sum;
+    return -1;
 }
 
