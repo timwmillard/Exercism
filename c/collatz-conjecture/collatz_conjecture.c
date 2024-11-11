5c5af8acda3cd7f4c0c2e96dfa92bb448f49da75
Tim Millard <timwmillard@gmail.com>
2024-11-12 09:16:22 +1100

    C collatz conjecture solved

--- c/collatz-conjecture/collatz_conjecture.c
+++ c/collatz-conjecture/collatz_conjecture.c
@@ -1,1 +1,17 @@
 #include "collatz_conjecture.h"
+
+int steps_count(int n, int s) {
+    if (n <= 0) return ERROR_VALUE;
+
+    if (n == 1) return s;
+
+    if (n%2 == 0) // even
+        return steps_count(n/2, ++s);
+    
+    return steps_count(3*n + 1, ++s);
+}
+
+int steps(int start) {
+    return steps_count(start, 0);
+}
+
