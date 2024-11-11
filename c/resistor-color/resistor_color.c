ddce90e248f24cef18bb029b3fc0eb1568ac129d
Tim Millard <timwmillard@gmail.com>
2024-11-12 08:25:43 +1100

    C resitor color solved 2

--- c/resistor-color/resistor_color.c
+++ c/resistor-color/resistor_color.c
@@ -1,16 +1,12 @@
-#include <stdlib.h>
 #include <stdint.h>
 #include "resistor_color.h"
+
+static resistor_band_t all_colors[] = { COLORS };
 
  int16_t color_code(resistor_band_t band) {
      return band;
  }
 
  resistor_band_t *colors(void) {
-     
-     resistor_band_t *cs = malloc(sizeof(resistor_band_t) * (WHITE + 1));
-     for (int i = 0; i <= WHITE; i++) {
-         cs[i] = (resistor_band_t) i;
-     }
-     return cs;
+     return all_colors;
  }
--- c/resistor-color/resistor_color.h
+++ c/resistor-color/resistor_color.h
@@ -3,17 +3,10 @@
 
 #include <stdint.h>
 
+#define COLORS BLACK, BROWN, RED, ORANGE, YELLOW, GREEN, BLUE, VIOLET, GREY, WHITE
+
 typedef enum {
-    BLACK = 0,
-    BROWN = 1,
-    RED = 2,
-    ORANGE = 3,
-    YELLOW = 4,
-    GREEN = 5,
-    BLUE = 6,
-    VIOLET = 7,
-    GREY = 8,
-    WHITE = 9,
+    COLORS
 } resistor_band_t;
 
 int16_t color_code(resistor_band_t band);
