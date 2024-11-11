0eefd056b929756216c003ad6fc3002e96d67972
Tim Millard <timwmillard@gmail.com>
2024-11-11 19:43:25 +1100

    C resitor color problem

---
+++ c/resistor-color/resistor_color.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdint.h>
+#include "resistor_color.h"
+
+ int16_t color_code(resistor_band_t band) {
+     return band;
+ }
+
+ resistor_band_t *colors(void) {
+     return NULL;
+ }
