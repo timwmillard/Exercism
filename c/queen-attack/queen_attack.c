20953452d4a20441b897f8f7471bf3966842d6b5
Tim Millard <timwmillard@gmail.com>
2024-11-12 10:03:38 +1100

    C queen attack problem

---
+++ c/queen-attack/queen_attack.c
@@ -0,0 +1,23 @@
+#include <stdbool.h>
+
+#include "queen_attack.h"
+
+bool valid_position(position_t pos) {
+    return pos.row >= 0 && pos.row < 8 &&
+        pos.column >= 0 && pos.column < 8;
+}
+
+bool position_equal(position_t pos1, position_t pos2) {
+    return pos1.row == pos2.row && pos1.column == pos2.column;
+}
+
+attack_status_t can_attack(position_t queen_1, position_t queen_2) {
+    if (!valid_position(queen_1) || !valid_position(queen_2) || position_equal(queen_1, queen_2))
+            return INVALID_POSITION;
+
+    if (queen_1.row == queen_2.row &&
+            queen_1.column == queen_2.column)
+        return CAN_ATTACK;
+
+    return CAN_NOT_ATTACK;
+}
