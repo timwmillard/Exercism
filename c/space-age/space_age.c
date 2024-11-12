6511c9002ef880eccb817667a1e74dc1501ca993
Tim Millard <timwmillard@gmail.com>
2024-11-12 21:55:37 +1100

    C space age solved

--- c/space-age/space_age.c
+++ c/space-age/space_age.c
@@ -1,1 +1,20 @@
 #include "space_age.h"
+
+#define EARTH_SEC 31557600.0
+
+static float periods[] = {
+      0.2408467,
+      0.61519726,
+      1.0,
+      1.8808158,
+     11.862615,
+     29.447498,
+     84.016846,
+    164.79132
+};
+
+float age(planet_t planet, int64_t seconds) {
+    if (planet < MERCURY || planet > NEPTUNE) return -1;
+
+    return seconds / EARTH_SEC / periods[planet];
+}
