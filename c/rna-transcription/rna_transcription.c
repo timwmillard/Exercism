568decd33602fae559e33f683ce6d1a0dae81ed6
Tim Millard <timwmillard@gmail.com>
2024-11-12 22:23:05 +1100

    C rna transcription solved

--- c/rna-transcription/rna_transcription.c
+++ c/rna-transcription/rna_transcription.c
@@ -1,5 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+
 #include "rna_transcription.h"
 
 char *to_rna(const char *dna) {
-    return 0;
+    size_t len = strlen(dna);
+    char *rna = malloc(len+1);
+    
+    for (size_t i = 0; i < len; i++) {
+        switch (dna[i]) {
+            case 'G': rna[i] = 'C'; break;
+            case 'C': rna[i] = 'G'; break;
+            case 'T': rna[i] = 'A'; break;
+            case 'A': rna[i] = 'U'; break;
+            default: rna[i] = '\0';
+        }
+    }
+    rna[len] = '\0';
+    return rna;
 }
+
