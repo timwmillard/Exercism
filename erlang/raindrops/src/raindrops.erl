0d784c03aeba40405b14ca67f3347907970e44c5
Tim Millard <timwmillard@gmail.com>
2024-11-12 21:27:27 +1100

    Erlang raindrops solved

--- erlang/raindrops/src/raindrops.erl
+++ erlang/raindrops/src/raindrops.erl
@@ -2,5 +2,9 @@
 
 -export([convert/1]).
 
+convert(Number) -> 
+    Result1 = if Number rem 3 == 0 -> "Pling"; true -> [] end,
+    Result2 = if Number rem 5 == 0 -> Result1 ++ "Plang"; true -> Result1 end,
+    Result3 = if Number rem 7 == 0 -> Result2 ++ "Plong"; true -> Result2 end,
+    if Result3 == "" -> integer_to_list(Number); true ->Result3 end.
 
-convert(_Number) -> undefined.
