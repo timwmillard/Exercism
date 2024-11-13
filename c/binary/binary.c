fbd61f2c9dcbc53c0894a62585649a561e3c128d
Tim Millard <timwmillard@gmail.com>
2024-11-13 17:22:49 +1100

    C binary solved

--- c/binary/binary.c
+++ c/binary/binary.c
@@ -1,1 +1,18 @@
+#include <string.h>
+
 #include "binary.h"
+
+int convert(const char *input)
+{
+    size_t len = strlen(input);
+    int num = 0;
+    for (size_t i = 0; i < len; i++) {
+        if (input[i] == '1') {
+           int n = 1 << (len - i - 1); 
+           num += n;
+        } else if (input[i] != '0')
+            return INVALID;
+    }
+    return num;
+}
+
