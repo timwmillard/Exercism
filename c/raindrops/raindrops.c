588a06e400791bbb945c41f5b823302dc1e32652
Tim Millard <timwmillard@gmail.com>
2024-11-13 22:50:22 +1100

    C raindrops solved

--- c/raindrops/raindrops.c
+++ c/raindrops/raindrops.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
 #include "raindrops.h"
+
+void convert(char result[], int drops)
+{
+    if (drops % 3 == 0) strcat(result, "Pling");
+    if (drops % 5 == 0) strcat(result, "Plang");
+    if (drops % 7 == 0) strcat(result, "Plong");
+
+    if (strlen(result) < 1) {
+       sprintf(result, "%d", drops); 
+    }
+}
+
