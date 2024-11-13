f03ee4cb94b4d51fc202620091ea5506fbcc3609
Tim Millard <timwmillard@gmail.com>
2024-11-13 13:15:14 +1100

    Erlang acronym solved

--- erlang/acronym/src/acronym.erl
+++ erlang/acronym/src/acronym.erl
@@ -2,5 +2,21 @@
 
 -export([abbreviate/1]).
 
+%| Input                     | Output |
+%| ------------------------- | ------ |
+%| As Soon As Possible       | ASAP   |
+%| Liquid-crystal display    | LCD    |
+%| Thank George It's Friday! | TGIF   |
 
-abbreviate(_Phrase) -> undefined.
+is_break(Letter) -> Letter == $  orelse Letter == $_ orelse Letter == $-.
+
+abbreviate(Phrase) -> abbreviate(Phrase, [], true).
+
+abbreviate([], Abb, _) -> Abb;
+abbreviate([Letter | Rest], Abb, New) -> 
+    IsBreak = is_break(Letter), 
+    if 
+        New andalso not IsBreak -> abbreviate(Rest, Abb ++ [string:to_upper(Letter)], false);
+        true -> abbreviate(Rest, Abb, IsBreak)
+    end.
+
