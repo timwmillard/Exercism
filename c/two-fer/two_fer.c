6a0cd584be8586453e84cd052eccd8b5e3e23edf
Tim Millard <timwmillard@gmail.com>
2024-11-13 22:38:14 +1100

    C two-fer solved

--- c/two-fer/two_fer.c
+++ c/two-fer/two_fer.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 #include "two_fer.h"
+
+void two_fer(char *buffer, const char *name)
+{
+    if (name == NULL) name = "you";
+    sprintf(buffer, "One for %s, one for me.", name);
+}
+
